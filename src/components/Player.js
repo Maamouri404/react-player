c4dac06ad2afa1493d523363aeda4e78d05a2f07
Maamouri404 <email@example.com>
2021-03-12 01:52:21 +0100

    2

---
+++ src/components/Player.js
@@ -0,0 +1,9 @@
+const Player = () => {
+  return (
+    <div className="player">
+      <h1>Player</h1>
+      <h2>Componenet</h2>
+    </div>
+  );
+};
+export default Player;
---
+++ src/components/Song.js
@@ -0,0 +1,9 @@
+const Song = () => {
+  return (
+    <div className="Song">
+      <h1>Song</h1>
+      <h2>Componenet</h2>
+    </div>
+  );
+};
+export default Song;
